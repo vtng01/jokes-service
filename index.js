19de0c07ebddd03862b77ad9d7b67c458efb221d
Van Nguyen <vanthoai@fb.com>
2023-04-05 12:58:07 +0100

    passed all tests

--- index.js
+++ index.js
@@ -1,18 +1,33 @@
-const express = require('express');
+const express = require("express");
 const app = express();
-const { Joke } = require('./db');
+const { Joke } = require("./db");
+const { Op } = require("sequelize");
 
 app.use(express.json());
-app.use(express.urlencoded({extended:true}));
+app.use(express.urlencoded({ extended: true }));
 
-app.get('/jokes', async (req, res, next) => {
+app.get("/jokes", async (req, res, next) => {
   try {
     // TODO - filter the jokes by tags and content
-    const jokes = [];
+    /**
+     * where: {
+     * tags: {[Op.like]: %tags%},
+     * joke: {[Op.like]: %content%}
+     * }
+     */
+    const { tags, content } = req.query;
+    const constraint = {};
+    if (tags) {
+      constraint.tags = { [Op.like]: `%${tags}%` };
+    }
+    if (content) {
+      constraint.joke = { [Op.like]: `%${content}%` };
+    }
+    const jokes = await Joke.findAll({ where: constraint });
     res.send(jokes);
   } catch (error) {
     console.error(error);
-    next(error)
+    next(error);
   }
 });
 
